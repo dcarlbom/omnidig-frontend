1f7797f3b70c64a16e80ae4f4050aad606b257c8
Daniel Carlbom <daniel@omnidig.se>
2022-02-02 00:09:09 +0100

    initial footer

---
+++ components/Footer.js
@@ -0,0 +1,125 @@
+import next from "next"
+import Link from "next/link"
+import styled from "styled-components"
+
+const Footer = () => {
+    return (
+        <FooterWrapper>
+            <SectionMain>
+                <MainItem>
+                    <SectionMainHeader>
+                        Våra Tjänster
+                    </SectionMainHeader>
+                    <MenuList>
+                        <li><a href="#">SEO</a></li>
+                        <li><a href="#">Google Analytics</a></li>
+                        <li><a href="#">Google Tag Manager</a></li>
+                        <li><a href="#">Google Ads</a></li>
+                        <li><a href="#">Webbdesign</a></li>
+                    </MenuList>
+                </MainItem>
+
+                <MainItem>
+                    <SectionMainHeader>
+                        Om oss
+                    </SectionMainHeader>
+                    <MenuList>
+                        <li><a href="#">Vårt Team</a></li>
+                        <li><a href="#">Om företaget</a></li>
+                    </MenuList>
+                </MainItem>
+
+                <MainItem>
+                    <SectionMainHeader>
+                        Läsvärt
+                    </SectionMainHeader>
+                    <MenuList>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                    </MenuList>
+                </MainItem>
+
+                <MainItem>
+                    <SectionMainHeader>
+                        Omnidig AB
+                    </SectionMainHeader>
+                    <MenuList>
+                        <li>Sälnaberg 553</li>
+                        <li>195 96 Rosersberg</li>
+                        
+                        <li>559029-0119</li>
+                    </MenuList>
+                </MainItem>
+            </SectionMain>
+            
+            <SectionLegal>
+                <LegalList>
+                    <LegalItem>Integritetspolicy</LegalItem>
+                    <LegalItem>Kontakta oss</LegalItem>
+                    <LegalItem>&copy; Omnidig AB</LegalItem>
+                </LegalList>
+            </SectionLegal>
+        </FooterWrapper>
+    )
+}
+
+export default Footer
+
+const FooterWrapper = styled.footer`
+    background-color: #555;
+    color: #bbb;
+    line-height: 1.5rem;
+`
+
+const MainItem = styled.div`
+    padding: 1.25rem;
+    min-width: 12.5rem;
+`
+
+const SectionMain = styled.section`
+    padding: 1.25rem 3.875rem;
+    display: flex;
+    flex-wrap: wrap;
+    @media (min-width: 768px) {
+        justify-content:space-around;
+    }
+    
+    @media (min-width: 1240px) {
+        justify-content:space-evenly;
+    }
+`
+
+const SectionMainHeader = styled.h3`
+    color: #fff;
+    font-size: 1.2rem;
+    padding-bottom: 0.625rem;
+`
+
+const MenuList = styled.ul`
+    list-style: none;
+    padding-left: 0;
+`
+
+const SectionLegal = styled.section`
+    padding: 0.9375rem 1.875rem;
+    background-color: #333;
+`
+
+const LegalList =styled.ul`
+    width:100%;
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+`
+
+const LegalItem = styled.li`
+    margin: 0.125rem 0.625rem;
+    white-space: nowrap;
+
+    &:nth-last-child(2){
+        flex:1;
+    }
+`
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import styled from 'styled-components'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
@@ -11,14 +12,16 @@ export default function Home() {
         <meta name="description" content="Static, fix this." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <TopBar>
+        Vi håller för närvarande på att bygga om omnidig.se - men du kan nå oss på 070-6050670.
+      </TopBar>
       <main className={styles.main}>
         <h1 className={styles.title}>
           OMNIDIG
         </h1>
 
         <p className={styles.description}>
-          Vi bygger om, snart tillbaka!
+          
         </p>
       </main>
 
@@ -29,7 +32,16 @@ export default function Home() {
   )
 }
 
-const Footer = styled.footer`
+const TopBar = styled.div`
+display: flex;
+-webkit-box-pack: center;
+justify-content: center;
+background: rgb(0,0,0);
+color: white;
+padding: 0.2rem 0;
+`
+
+const Foo = styled.footer`
   display: flex;
   flex: 1;
   padding: 2rem 0;
